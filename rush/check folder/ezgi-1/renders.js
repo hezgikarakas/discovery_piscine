071cfa817e2b53e4ffe7f267635cd639869337d4
H Ezgi Karakas <hezgikarakasschueller@gmail.com>
2023-03-12 01:28:41 +0100

    rush document

---
+++ rush/check folder/ezgi-1/renders.js
@@ -0,0 +1,15 @@
+const components = [
+	'navbar',
+	'home',
+	'aboutme',
+	'education',
+	'footer'
+];
+const path = './components';
+for (const name of components) {
+	fetch(`${path}/${name}.html`)
+		.then(response => response.text())
+		.then(data => {
+			document.querySelector(name).innerHTML = data;
+		}); 
+}
